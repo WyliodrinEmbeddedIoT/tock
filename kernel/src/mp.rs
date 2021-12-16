8dbf4337cd77702bbeb975e6df4e769ec9fff8c4
Alexandru Radovici <msg4alex@gmail.com>
2021-12-16 11:50:08 +0200

    messages

--- kernel/src/mp.rs
+++ kernel/src/mp.rs
@@ -1,9 +1,9 @@
-use crate::grant::Grant;
-use crate::process::{Error};
-use crate::processbuffer::{ReadWriteProcessBuffer, ReadOnlyProcessBuffer, ReadableProcessBuffer};
+use crate::grant::{AllowRoCount, AllowRwCount, Grant, UpcallCount};
+use crate::process::Error;
+use crate::processbuffer::{ReadOnlyProcessBuffer, ReadWriteProcessBuffer, ReadableProcessBuffer};
 use crate::syscall::{CommandReturn, SyscallDriver};
-use crate::{ErrorCode, ProcessId};
 use crate::utilities::cells::TakeCell;
+use crate::{ErrorCode, ProcessId};
 
 enum MessageStatus {
     Ready = 0b00,
@@ -30,44 +30,45 @@ const READ_COMMAND_MASK: usize = 0x40;
 const SEARCH_SERVICE_BUFFER: usize = 0;
 const REPLY_BUFFER: usize = 1;
 
+mod allow_ro {
+    pub const SEARCH_SERVICE_BUFFER: usize = 0;
+    pub const COUNT: usize = 1;
+}
+
+// NUM_BUFFERS has to be NUM_PROCS * 2
+pub type MpGrant<const NUM_PROCS: usize, const NUM_BUFFERS: usize> = Grant<
+    ProcessStorage,
+    UpcallCount<NUM_PROCS>,
+    AllowRoCount<{ allow_ro::COUNT }>,
+    AllowRwCount<NUM_BUFFERS>,
+>;
+
 struct MessageHeader {
     value: usize,
     len: usize,
 }
 
-pub struct ProcessStorage<const NUM_PROCS: usize> {
-    buffers: [ReadWriteProcessBuffer; NUM_PROCS],
-    read_only_buffers: [ReadOnlyProcessBuffer; 2],
-    sent_messages: [Option<MessageHeader>; NUM_PROCS],
-    received_messages: [Option<MessageHeader>; NUM_PROCS],
+#[derive(Default)]
+pub struct ProcessStorage {
     send_status: u64,
     receive_status: u32,
 }
 
-impl<const NUM_PROCS: usize> Default for ProcessStorage<NUM_PROCS> {
-    fn default() -> ProcessStorage<NUM_PROCS> {
-        const EMPTY_BUFFER: ReadWriteProcessBuffer = ReadWriteProcessBuffer::const_default();
-        const EMPTY_MESSAGE_HEADER: Option<MessageHeader> = None;
-        ProcessStorage {
-            buffers: [EMPTY_BUFFER; NUM_PROCS],
-            read_only_buffers: [ReadOnlyProcessBuffer::default(), ReadOnlyProcessBuffer::default()],
-            sent_messages: [EMPTY_MESSAGE_HEADER; NUM_PROCS],
-            received_messages: [EMPTY_MESSAGE_HEADER; NUM_PROCS],
-            send_status: 0,
-            receive_status: 0,
-        }
-    }
-}
-
-pub struct Mp<const NUM_PROCS: usize> {
-    processes: Grant<ProcessStorage<NUM_PROCS>, NUM_PROCS>,
+pub struct Mp<const NUM_PROCS: usize, const NUM_BUFFERS: usize> {
+    processes: MpGrant<NUM_PROCS, NUM_BUFFERS>,
     identifiers: TakeCell<'static, [Option<ProcessId>; NUM_PROCS]>,
 }
 
-impl<const NUM_PROCS: usize> Mp<NUM_PROCS> {
-    pub fn new(processes: Grant<ProcessStorage<NUM_PROCS>, NUM_PROCS>, identifiers: &'static mut [Option<ProcessId>; NUM_PROCS]) -> Mp<NUM_PROCS> {
+impl<const NUM_PROCS: usize, const NUM_BUFFERS: usize> Mp<NUM_PROCS, NUM_BUFFERS> {
+    pub fn new(
+        processes: MpGrant<NUM_PROCS, NUM_BUFFERS>,
+        identifiers: &'static mut [Option<ProcessId>; NUM_PROCS],
+    ) -> Mp<NUM_PROCS, NUM_BUFFERS> {
         assert!(NUM_PROCS <= 32);
-        Mp { processes, identifiers: TakeCell::new(identifiers) }
+        Mp {
+            processes,
+            identifiers: TakeCell::new(identifiers),
+        }
     }
 
     fn get_sent_message_status(sent_status: u64, destination_index: usize) -> MessageStatus {
@@ -75,7 +76,11 @@ impl<const NUM_PROCS: usize> Mp<NUM_PROCS> {
         MessageStatus::from(status as u8)
     }
 
-    fn set_sent_message_status(sent_status: &mut u64, destination_index: usize, status: MessageStatus) {
+    fn set_sent_message_status(
+        sent_status: &mut u64,
+        destination_index: usize,
+        status: MessageStatus,
+    ) {
         *sent_status &= !(0b11 << (destination_index * 2));
         *sent_status |= (status as u64) << (destination_index * 2);
     }
@@ -95,64 +100,32 @@ impl<const NUM_PROCS: usize> Mp<NUM_PROCS> {
     fn get_identifier(&self, process: ProcessId) -> u32 {
         let mut id = 0;
         self.filter_identifiers();
-        self.identifiers.map(|identifiers| {
-            for (index, identifier) in identifiers.iter().enumerate() {
-                if let Some(process_id) = identifier {
-                    if process_id.id() == process.id() {
+        self.identifiers
+            .map(|identifiers| {
+                for (index, identifier) in identifiers.iter().enumerate() {
+                    if let Some(process_id) = identifier {
+                        if process_id.id() == process.id() {
+                            id = index;
+                            return;
+                        }
+                    }
+                }
+                for (index, identifier) in identifiers.iter_mut().enumerate() {
+                    if let None = identifier {
+                        *identifier = Some(process);
                         id = index;
                         return;
                     }
                 }
-            }
-            for (index, identifier) in identifiers.iter_mut().enumerate() {
-                if let None = identifier {
-                    *identifier = Some(process);
-                    id = index;
-                    return;
-                }
-            }
-        }).expect("unable set a mp identifier");
+            })
+            .expect("unable set a mp identifier");
         id as u32
     }
 }
 
-impl<const NUM_PROCS: usize> SyscallDriver for Mp<NUM_PROCS> {
-    fn allow_readwrite(
-        &self,
-        process_id: ProcessId,
-        allow_num: usize,
-        mut buffer: ReadWriteProcessBuffer,
-    ) -> Result<ReadWriteProcessBuffer, (ReadWriteProcessBuffer, ErrorCode)> {
-        if allow_num < NUM_PROCS {
-            match self.processes.enter(process_id, |process, _| {
-                core::mem::swap(&mut process.buffers[allow_num], &mut buffer);
-            }) {
-                Ok(()) => Ok(buffer),
-                Err(err) => Err((buffer, err.into())),
-            }
-        } else {
-            Err((buffer, ErrorCode::NOSUPPORT))
-        }
-    }
-
-    fn allow_readonly(
-        &self,
-        process_id: ProcessId,
-        allow_num: usize,
-        mut buffer: ReadOnlyProcessBuffer,
-    ) -> Result<ReadOnlyProcessBuffer, (ReadOnlyProcessBuffer, ErrorCode)> {
-        if allow_num < 2 {
-            match self.processes.enter(process_id, |process, _| {
-                core::mem::swap(&mut process.read_only_buffers[allow_num], &mut buffer);
-            }) {
-                Ok(()) => Ok(buffer),
-                Err(err) => Err((buffer, err.into())),
-            }
-        } else {
-            Err((buffer, ErrorCode::NOSUPPORT))
-        }
-    }
-
+impl<const NUM_PROCS: usize, const NUM_BUFFERS: usize> SyscallDriver
+    for Mp<NUM_PROCS, NUM_BUFFERS>
+{
     fn command(
         &self,
         command_num: usize,
@@ -164,29 +137,47 @@ impl<const NUM_PROCS: usize> SyscallDriver for Mp<NUM_PROCS> {
             0 => CommandReturn::success(),
             // search for process
             1 => {
-                let res = self.processes.enter(process_id, |process, _| {
-                    process.read_only_buffers[SEARCH_SERVICE_BUFFER].enter(|buffer| {
-                        match self.processes.kernel.process_until(|p| {
-                            let s = p.get_process_name().as_bytes();
-                                        // are slices equal?
-                                        if s.len() == buffer.len()
-                                            && s.iter()
-                                                .zip(buffer.iter())
-                                                .all(|(c1, c2)| *c1 == c2.get())
-                                        {
-                                            Some(self.get_identifier(p.processid()))
-                                        } else {
-                                            None
+                let res = self
+                    .processes
+                    .enter(process_id, |process, kernel_grant| {
+                        kernel_grant
+                            .get_readwrite_processbuffer(allow_ro::SEARCH_SERVICE_BUFFER)
+                            .and_then(|buffer_ref| {
+                                buffer_ref
+                                    .enter(|buffer| {
+                                        match self.processes.kernel.process_until(|p| {
+                                            let s = p.get_process_name().as_bytes();
+                                            // are slices equal?
+                                            if s.len() == buffer.len()
+                                                && s.iter()
+                                                    .zip(buffer.iter())
+                                                    .all(|(c1, c2)| *c1 == c2.get())
+                                            {
+                                                Some(self.get_identifier(p.processid()))
+                                            } else {
+                                                None
+                                            }
+                                        }) {
+                                            Some(id) => Ok(id),
+                                            None => Err(ErrorCode::NODEVICE),
                                         }
-                        }) {
-                            Some(id) => Ok(id),
-                            None => Err(ErrorCode::NODEVICE)
-                        }
-                    }).map_err(|err| err.into()).and_then(|x| x)
-                }).map_err(|err| err.into()).and_then(|x| x);
+                                    })
+                                    .map_err(|error| error.into())
+                                    .and_then(|x| x)
+                            })
+                            .map_err(|x| x.into())
+                        // process.read_only_buffers[SEARCH_SERVICE_BUFFER]
+                        //     .enter(|buffer| {
+                        //
+                        //     })
+                        //     .map_err(|err| err.into())
+                        //     .and_then(|x| x)
+                    })
+                    .map_err(|err| err.into())
+                    .and_then(|x| x);
                 match res {
                     Ok(id) => CommandReturn::success_u32(id),
-                    Err(err) => CommandReturn::failure(err) 
+                    Err(err) => CommandReturn::failure(err),
                 }
             }
             // Send command
