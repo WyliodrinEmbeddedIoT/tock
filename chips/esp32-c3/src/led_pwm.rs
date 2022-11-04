eeca123707c5f225dbf4d55a0c17154e08e33f5b
CosminGGeorgescu <cosmingg2013@gmail.com>
2022-11-04 14:20:57 +0200

    finished Timers part + cleaner code

--- chips/esp32-c3/src/led_pwm.rs
+++ chips/esp32-c3/src/led_pwm.rs
@@ -1,4 +1,6 @@
-use kernel::utilities::registers::interfaces::ReadWriteable;
+use core::convert::From;
+use core::panic;
+use kernel::utilities::registers::interfaces::{ReadWriteable, Readable, Writeable};
 use kernel::utilities::registers::{register_bitfields, register_structs, ReadWrite};
 use kernel::utilities::StaticRef;
 use kernel::ErrorCode;
@@ -7,46 +9,23 @@ pub const LED_PWM_BASE: StaticRef<LedPwmRegisters> =
     unsafe { StaticRef::new(0x6001_9000 as *const LedPwmRegisters) };
 
 register_structs! {
+    pub LedPwmChannel {
+        (0x0000 => conf0: ReadWrite<u32, LEDC_CH_CONF0::Register>),
+        (0x0004 => hpoint: ReadWrite<u32, LEDC_CH_HPOINT::Register>),
+        (0x0008 => duty: ReadWrite<u32, LEDC_CH_DUTY::Register>),
+        (0x000C => conf1: ReadWrite<u32, LEDC_CH_CONF1::Register>),
+        (0x0010 => duty_r: ReadWrite<u32, LEDC_CH_DUTY_R::Register>),
+        (0x0014 => @END),
+    },
+    pub LedPwmTimer {
+        (0x0000 => conf: ReadWrite<u32, LEDC_TIMER_CONF::Register>),
+        (0x0004 => value: ReadWrite<u32, LEDC_TIMER_VALUE::Register>),
+        (0x0008 => @END),
+    },
     pub LedPwmRegisters { //<- possible naming or "LedcRegisters"
-        (0x0000 => ledc_ch0_conf0: ReadWrite<u32, LEDC_CH_CONF0::Register>),
-        (0x0004 => ledc_ch0_hpoint: ReadWrite<u32, LEDC_CH_HPOINT::Register>),
-        (0x0008 => ledc_ch0_duty: ReadWrite<u32, LEDC_CH_DUTY::Register>),
-        (0x000C => ledc_ch0_conf1: ReadWrite<u32, LEDC_CH_CONF1::Register>),
-        (0x0010 => ledc_ch0_duty_r: ReadWrite<u32, LEDC_CH_DUTY_R::Register>),
-        (0x0014 => ledc_ch1_conf0: ReadWrite<u32, LEDC_CH_CONF0::Register>),
-        (0x0018 => ledc_ch1_hpoint: ReadWrite<u32, LEDC_CH_HPOINT::Register>),
-        (0x001C => ledc_ch1_duty: ReadWrite<u32, LEDC_CH_DUTY::Register>),
-        (0x0020 => ledc_ch1_conf1: ReadWrite<u32, LEDC_CH_CONF1::Register>),
-        (0x0024 => ledc_ch1_duty_r: ReadWrite<u32, LEDC_CH_DUTY_R::Register>),
-        (0x0028 => ledc_ch2_conf0: ReadWrite<u32, LEDC_CH_CONF0::Register>),
-        (0x002C => ledc_ch2_hpoint: ReadWrite<u32, LEDC_CH_HPOINT::Register>),
-        (0x0030 => ledc_ch2_duty: ReadWrite<u32, LEDC_CH_DUTY::Register>),
-        (0x0034 => ledc_ch2_conf1: ReadWrite<u32, LEDC_CH_CONF1::Register>),
-        (0x0038 => ledc_ch2_duty_r: ReadWrite<u32, LEDC_CH_DUTY_R::Register>),
-        (0x003C => ledc_ch3_conf0: ReadWrite<u32, LEDC_CH_CONF0::Register>),
-        (0x0040 => ledc_ch3_hpoint: ReadWrite<u32, LEDC_CH_HPOINT::Register>),
-        (0x0044 => ledc_ch3_duty: ReadWrite<u32, LEDC_CH_DUTY::Register>),
-        (0x0048 => ledc_ch3_conf1: ReadWrite<u32, LEDC_CH_CONF1::Register>),
-        (0x004C => ledc_ch3_duty_r: ReadWrite<u32, LEDC_CH_DUTY_R::Register>),
-        (0x0050 => ledc_ch4_conf0: ReadWrite<u32, LEDC_CH_CONF0::Register>),
-        (0x0054 => ledc_ch4_hpoint: ReadWrite<u32, LEDC_CH_HPOINT::Register>),
-        (0x0058 => ledc_ch4_duty: ReadWrite<u32, LEDC_CH_DUTY::Register>),
-        (0x005C => ledc_ch4_conf1: ReadWrite<u32, LEDC_CH_CONF1::Register>),
-        (0x0060 => ledc_ch4_duty_r: ReadWrite<u32, LEDC_CH_DUTY_R::Register>),
-        (0x0064 => ledc_ch5_conf0: ReadWrite<u32, LEDC_CH_CONF0::Register>),
-        (0x0068 => ledc_ch5_hpoint: ReadWrite<u32, LEDC_CH_HPOINT::Register>),
-        (0x006C => ledc_ch5_duty: ReadWrite<u32, LEDC_CH_DUTY::Register>),
-        (0x0070 => ledc_ch5_conf1: ReadWrite<u32, LEDC_CH_CONF1::Register>),
-        (0x0074 => ledc_ch5_duty_r: ReadWrite<u32, LEDC_CH_DUTY_R::Register>),
+        (0x0000 => ledc_ch: [LedPwmChannel; 6]),
         (0x0078 => _reserved0),
-        (0x00A0 => ledc_timer0_conf: ReadWrite<u32, LEDC_TIMER_CONF::Register>),
-        (0x00A4 => ledc_timer0_value: ReadWrite<u32, LEDC_TIMER_VALUE::Register>),
-        (0x00A8 => ledc_timer1_conf: ReadWrite<u32, LEDC_TIMER_CONF::Register>),
-        (0x00AC => ledc_timer1_value: ReadWrite<u32, LEDC_TIMER_VALUE::Register>),
-        (0x00B0 => ledc_timer2_conf: ReadWrite<u32, LEDC_TIMER_CONF::Register>),
-        (0x00B4 => ledc_timer2_value: ReadWrite<u32, LEDC_TIMER_VALUE::Register>),
-        (0x00B8 => ledc_timer3_conf: ReadWrite<u32, LEDC_TIMER_CONF::Register>),
-        (0x00BC => ledc_timer3_value: ReadWrite<u32, LEDC_TIMER_VALUE::Register>),
+        (0x00A0 => ledc_timer: [LedPwmTimer; 4]),
         (0x00C0 => ledc_int_raw: ReadWrite<u32, LEDC_INT::Register>),
         (0x00C4 => ledc_int_st: ReadWrite<u32, LEDC_INT::Register>),
         (0x00C8 => ledc_int_ena: ReadWrite<u32, LEDC_INT::Register>),
@@ -59,28 +38,31 @@ register_structs! {
 }
 
 register_bitfields! [u32,
-    //($x) value used for timers, value between [0, 3]
-    //($n) value used for pwm generators, value between [0, 5]
+    //x used for timers, value between [0, 3]
+    //n used for pwm generators, value between [0, 5]
 
     //naming suggestions are appreciated
     LEDC_CH_CONF0 [
-        //used to select one of the timers on channel ($n)
-        //($n): select Timer($n)
+        //used to select one of the timers on channel n
+        //0: select Timer0
+        //1: select Timer1
+        //2: select Timer2
+        //3: select Timer3
         TIMER_SEL_CH OFFSET(0) NUMBITS(2) [],
-        //set to enable signal output on channel ($n)
+        //set to enable signal output on channel n
         SIG_OUT_EN_CH OFFSET(2) NUMBITS(1) [],
-        //used to control the output value when channel ($n) is inactive(LEDC_SIG_OUT_EN_CH($n) == 0)
+        //used to control the output value when channel n is inactive(LEDC_SIG_OUT_EN_CHn == 0)
         IDLE_LV_CH OFFSET(3) NUMBITS(1) [],
-        //used to update the listed fields below for channel ($n); is automatically cleared by hardware
+        //used to update the listed fields below for channel n; is automatically cleared by hardware
         //HPOINT_CHn, DUTY_START_CHn, SIG_OUT_EN_CHn, TIMER_SEL_CHn, DUTY_NUM_CHn, DUTY_CYCLE_CHn,
         //DUTY_SCALE_CHn, DUTY_INC_CHn and OVF_CNT_EN_CHn
         PARA_UP_CH OFFSET(4) NUMBITS(1) [],
         //used to configure maximum times of overflow - 1
-        //LEDC_OVF_CNT_CH($n)_INT will be triggered when channel ($n) overflows for (LEDC_OVF_NUM_CH($n) + 1) times
+        //LEDC_OVF_CNT_CHn_INT will be triggered when channel n overflows for (LEDC_OVF_NUM_CHn + 1) times
         OVF_NUM_CH OFFSET(5) NUMBITS(10) [],
-        //used to count the number of times when the timer selected by channel ($n) overflows
+        //used to count the number of times when the timer selected by channel n overflows
         OVF_CNT_EN_CH OFFSET(15) NUMBITS(1) [],
-        //set to reset timer-overflow counter of channel ($n)
+        //set to reset timer-overflow counter of channel n
         OVF_CNT_RESET_CH OFFSET(16) NUMBITS(1) [],
     ],
     LEDC_CH_CONF1 [
@@ -92,11 +74,10 @@ register_bitfields! [u32,
     ],
     LEDC_CONF [
         //used to select the clock source for all 4 timers
-        APB_CLK_SEL OFFSET(0) NUMBITS(2) [
-            APB_CLK = 1,
-            FOSC_CLK = 2,
-            XTAL_CLK = 3,
-        ],
+        //01: APB_CLK
+        //10: FOSC_CLK
+        //11: XTAL_CLK
+        APB_CLK_SEL OFFSET(0) NUMBITS(2) [],
         //used to control clock
         //0: support clock only when application writes registers
         //1: force clock on register
@@ -112,45 +93,45 @@ register_bitfields! [u32,
         DUTY_R_CH OFFSET(0) NUMBITS(19) [],
     ],
     LEDC_TIMER_CONF [
-        //used to control range of counter of timer ($x)
+        //used to control range of counter of timer x
         TIMER_DUTY_RES OFFSET(0) NUMBITS(4) [],
-        //used to configure the divisor for the divider of timer ($x)
+        //used to configure the divisor for the divider of timer x
         CLK_DIV_TIMER OFFSET(4) NUMBITS(18) [],
-        //used to suspend the counter of timer ($x)
+        //used to suspend the counter of timer x
         TIMER_PAUSE OFFSET(22) NUMBITS(1) [],
-        //used to reset timer ($x); counter will show 0 after reset
+        //used to reset timer x; counter will show 0 after reset
         TIMER_RST OFFSET(23) NUMBITS(1) [],
-        //set to update LEDC_CLK_DIV_TIMER($x) and LEDC_TIMER($x)_DUTY_RES
+        //set to update LEDC_CLK_DIV_TIMERx and LEDC_TIMERx_DUTY_RES
         TIMER_PARA_UP OFFSET(25) NUMBITS(1) [],
     ],
     LEDC_TIMER_VALUE [
-        //stores the current counter value of timer ($x)
+        //stores the current counter value of timer x
         TIMER_CNT OFFSET(0) NUMBITS(14) [],
     ],
     LEDC_INT [
         //RAW:
-        //triggered when timer ($x) has reached its maximum counter value
+        //triggered when timer x has reached its maximum counter value
         //ST:
-        //masked interrupt status bit for the LEDC_TIMER($x)_OVF_INT interrupt
-        //when LEDC_TIMER($x)_OVF_INT_ENA is set to 1
+        //masked interrupt status bit for the LEDC_TIMERx_OVF_INT interrupt
+        //when LEDC_TIMERx_OVF_INT_ENA is set to 1
         //ENA:
-        //set to enable LEDC_TIMER($x)_OVF_INT interrupt
+        //set to enable LEDC_TIMERx_OVF_INT interrupt
         //CLR:
-        //set to clear LEDC_TIMER($x)_OVF_INT interrupt
+        //set to clear LEDC_TIMERx_OVF_INT interrupt
         TIMER0_OVF OFFSET(0) NUMBITS(1) [],
         TIMER1_OVF OFFSET(1) NUMBITS(1) [],
         TIMER2_OVF OFFSET(2) NUMBITS(1) [],
         TIMER3_OVF OFFSET(3) NUMBITS(1) [],
         //RAW:
-        //interrupt raw bit for channel ($n)
+        //interrupt raw bit for channel n
         //triggered when the gradual change of duty has finished
         //ST:
-        //masked interrupt status bit for LEDC_DUTY_CHNG_END_CH($n)_INT interrupt
-        //when LEDC_DUTY_CHNG_CH($n)_INT_ENA is set to 1
+        //masked interrupt status bit for LEDC_DUTY_CHNG_END_CHn_INT interrupt
+        //when LEDC_DUTY_CHNG_CHn_INT_ENA is set to 1
         //ENA:
-        //set to enable LEDC_DUTY_CHNG_END_CH($n)_INT interrupt
+        //set to enable LEDC_DUTY_CHNG_END_CHn_INT interrupt
         //CLR:
-        //set to clear LEDC_DUTY_CHNG_END_CH($n)_INT interrupt
+        //set to clear LEDC_DUTY_CHNG_END_CHn_INT interrupt
         DUTY_CHNG_END_CH0 OFFSET(4) NUMBITS(1) [],
         DUTY_CHNG_END_CH1 OFFSET(5) NUMBITS(1) [],
         DUTY_CHNG_END_CH2 OFFSET(6) NUMBITS(1) [],
@@ -158,15 +139,15 @@ register_bitfields! [u32,
         DUTY_CHNG_END_CH4 OFFSET(8) NUMBITS(1) [],
         DUTY_CHNG_END_CH5 OFFSET(9) NUMBITS(1) [],
         //RAW:
-        //interrupt raw bit for channel ($n)
-        //triggered when overflow counter has reached value specified by LEDC_OVF_NUM_CH($n)
+        //interrupt raw bit for channel n
+        //triggered when overflow counter has reached value specified by LEDC_OVF_NUM_CHn
         //ST:
-        //masked interrupt status bit for LEDC_DUTY_CNT_CH($n)_INT interrupt
-        //when LEDC_OVF_CNT_CH($n)_INT_ENA is set to 1
+        //masked interrupt status bit for LEDC_DUTY_CNT_CHn_INT interrupt
+        //when LEDC_OVF_CNT_CHn_INT_ENA is set to 1
         //ENA:
-        //set to enable LEDC_OVF_CH($n)_INT interrupt
+        //set to enable LEDC_OVF_CHn_INT interrupt
         //CLR:
-        //set to clear LEDC_OVF_CNT_CH($n)_INT interrupt
+        //set to clear LEDC_OVF_CNT_CHn_INT interrupt
         OVF_CNT_CH0 OFFSET(10) NUMBITS(1) [],
         OVF_CNT_CH1 OFFSET(11) NUMBITS(1) [],
         OVF_CNT_CH2 OFFSET(12) NUMBITS(1) [],
@@ -180,8 +161,32 @@ register_bitfields! [u32,
     ]
 ];
 
+pub enum Timer {
+    Timer0 = 0,
+    Timer1 = 1,
+    Timer2 = 2,
+    Timer3 = 3,
+}
+
+#[derive(Copy, Clone)]
+pub enum Pwm {
+    Pwm0 = 0,
+    Pwm1 = 1,
+    Pwm2 = 2,
+    Pwm3 = 3,
+    Pwm4 = 4,
+    Pwm5 = 5,
+}
+
+pub enum ClockSource {
+    Apb = 1,
+    Fosc = 2,
+    Xtal = 3,
+}
+
 struct LedPwm {
     registers: StaticRef<LedPwmRegisters>,
+    //maybe add an array with the current specs for each timer and pwm ?
 }
 
 impl LedPwm {
@@ -191,77 +196,88 @@ impl LedPwm {
         }
     }
 
-    pub fn use_apb_clock_source(&self) {
+    pub fn configure_clk_source(&self, clock: ClockSource) {
         self.registers
             .ledc_conf
-            .modify(LEDC_CONF::APB_CLK_SEL::APB_CLK);
+            .modify(LEDC_CONF::APB_CLK_SEL.val(clock as u32));
     }
 
-    pub fn use_fosc_clk_source(&self) {
-        self.registers
-            .ledc_conf
-            .modify(LEDC_CONF::APB_CLK_SEL::FOSC_CLK);
-    }
+    //are the below Results needed ? panic or assert instead ?
 
-    pub fn use_xtal_clk_source(&self) {
-        self.registers
-            .ledc_conf
-            .modify(LEDC_CONF::APB_CLK_SEL::XTAL_CLK);
-    }
-
-    pub fn configure_clk_div_timer(
-        &self,
-        timer_number: u32,
-        //actual max size of 'a' is 10bits and 'b' is 8bits, but declared as u32 to avoid type mismatch
-        //could use u16 and u8 respectively and use 'as u32' in context; worth it ?
-        a: u32,
-        b: u32,
-    ) -> Result<(), ErrorCode> {
-        //^is the Result needed ?
+    //LEDC_CLK_DIV_TIMERx = A + B / 256
+    //A corresponds  to the most significant 10bits of LEDC_CLK_DIV_TIMERx
+    //B corresponds  to the least significant 8bits of LEDC_CLK_DIV_TIMERx
+    pub fn set_clock_diviser_value(&self, timer: Timer, a: u32, b: u32) -> Result<(), ErrorCode> {
         if a >= 1024 && b >= 256 {
-            //implemented as to fit in the bitfield
-            //SIZE or INVAL error ?
             Err(ErrorCode::SIZE)
         } else {
-            match timer_number {
-                0 => {
-                    self.registers
-                        .ledc_timer0_conf
-                        .modify(LEDC_TIMER_CONF::CLK_DIV_TIMER.val((a << 2) + b));
-                    self.registers
-                        .ledc_timer0_conf
-                        .modify(LEDC_TIMER_CONF::TIMER_PARA_UP::SET);
-                    Ok(())
-                }
-                1 => {
-                    self.registers
-                        .ledc_timer1_conf
-                        .modify(LEDC_TIMER_CONF::CLK_DIV_TIMER.val((a << 2) + b));
-                    self.registers
-                        .ledc_timer1_conf
-                        .modify(LEDC_TIMER_CONF::TIMER_PARA_UP::SET);
-                    Ok(())
-                }
-                2 => {
-                    self.registers
-                        .ledc_timer2_conf
-                        .modify(LEDC_TIMER_CONF::CLK_DIV_TIMER.val((a << 2) + b));
-                    self.registers
-                        .ledc_timer2_conf
-                        .modify(LEDC_TIMER_CONF::TIMER_PARA_UP::SET);
-                    Ok(())
-                }
-                3 => {
-                    self.registers
-                        .ledc_timer3_conf
-                        .modify(LEDC_TIMER_CONF::CLK_DIV_TIMER.val((a << 2) + b));
-                    self.registers
-                        .ledc_timer3_conf
-                        .modify(LEDC_TIMER_CONF::TIMER_PARA_UP::SET);
-                    Ok(())
-                }
-                _ => Err(ErrorCode::INVAL),
-            }
+            self.registers.ledc_timer[timer as usize]
+                .conf
+                .modify(LEDC_TIMER_CONF::CLK_DIV_TIMER.val((a << 2) + b));
+            self.registers.ledc_timer[timer as usize]
+                .conf
+                .modify(LEDC_TIMER_CONF::TIMER_PARA_UP::SET);
+            Ok(())
+        }
+    }
+
+    //The counter for each timer counts up to 2 ^ (LEDC_TIMERx_DUTY_RES) − 1,
+    //overflows and begins counting from 0 again
+
+    //the new configuration takes effect upon next overflow;
+    //implement the wait here or make the user somehow wait for the interrupt to be triggered ?
+    //see if the Result is needed further
+    pub fn configure_counter_range(&self, timer: Timer, exponent: u32) -> Result<(), ErrorCode> {
+        if exponent >= 16 {
+            Err(ErrorCode::SIZE)
+        } else {
+            self.registers.ledc_timer[timer as usize]
+                .conf
+                .modify(LEDC_TIMER_CONF::TIMER_DUTY_RES.val(exponent));
+            self.registers.ledc_timer[timer as usize]
+                .conf
+                .modify(LEDC_TIMER_CONF::TIMER_PARA_UP::SET);
+            Ok(())
+        }
+    }
+
+    pub fn configure_counter_overflow(
+        &self,
+        pwm: Pwm,
+        timer: Timer,
+        counter_overflows: u32,
+        timer_range_exponent: u32,
+    ) -> Result<(), ErrorCode> {
+        if counter_overflows >= 1024 {
+            Err(ErrorCode::SIZE)
+        } else {
+            //set the given timer as timer for the given pwm
+            self.registers.ledc_ch[pwm as usize]
+                .conf0
+                .modify(LEDC_CH_CONF0::TIMER_SEL_CH.val(timer as u32));
+            //update the TIMER_SEL_CH field
+            self.registers.ledc_ch[pwm as usize]
+                .conf0
+                .modify(LEDC_CH_CONF0::PARA_UP_CH::SET);
+
+            //enable the overflow counter
+            self.registers.ledc_ch[pwm as usize]
+                .conf0
+                .modify(LEDC_CH_CONF0::OVF_CNT_EN_CH::SET);
+
+            //set the number of counter overflows to generate an interrupt - 1
+            self.registers.ledc_ch[pwm as usize]
+                .conf0
+                .modify(LEDC_CH_CONF0::OVF_NUM_CH.val(counter_overflows));
+
+            //mask for indexing the counter overflow field for a specific channel
+            let mask: u32 = 0b0000_0000_0000_0000_0000_0100_0000_0000 << (pwm as usize);
+            //enable the counter overflow
+            self.registers
+                .ledc_int_ena
+                .set(self.registers.ledc_int_ena.get() | mask);
+            let _ = self.configure_counter_range(timer, timer_range_exponent);
+            Ok(())
         }
     }
 }
