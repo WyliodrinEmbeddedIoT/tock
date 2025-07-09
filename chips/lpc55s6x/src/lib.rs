e7863e63b63b0dcb5434ef0733ffeeafbac8ef18
genan2003 <genanomer99@gmail.com>
2025-07-09 10:30:49 +0300

    Adding support for NXP-LPC55

---
+++ chips/lpc55s6x/src/lib.rs
@@ -0,0 +1,35 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Tock Contributors 2025.
+
+#![no_std]
+
+use cortexm33::{initialize_ram_jump_to_main, unhandled_interrupt, CortexM33, CortexMVariant};
+
+extern "C" {
+    fn _estack();
+}
+
+#[cfg_attr(
+    all(target_arch = "arm", target_os = "none"),
+    link_section = ".vectors"
+)]
+#[cfg_attr(all(target_arch = "arm", target_os = "none"), used)]
+pub static BASE_VECTORS: [unsafe extern "C" fn(); 16] = [
+    _estack,
+    initialize_ram_jump_to_main,
+    unhandled_interrupt,
+    CortexM33::HARD_FAULT_HANDLER,
+    unhandled_interrupt,
+    unhandled_interrupt,
+    unhandled_interrupt,
+    unhandled_interrupt,
+    unhandled_interrupt,
+    unhandled_interrupt,
+    unhandled_interrupt,
+    CortexM33::SVC_HANDLER,
+    unhandled_interrupt,
+    unhandled_interrupt,
+    unhandled_interrupt,
+    CortexM33::SYSTICK_HANDLER,
+];
